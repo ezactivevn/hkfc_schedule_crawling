45da813d34c2685890f1ef43318eb2338e22ddbc
Hoang Phu <hoangphu@MacBook-Pro-cua-Hoang.local>
2023-10-17 09:15:33 +0700

    update action

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,40 +1,66 @@
+import json
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
+from selenium.webdriver.chrome.options import Options  # Import ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
+# Create ChromeOptions and set it to run in headless mode
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+# Khởi tạo trình duyệt (ví dụ: Google Chrome) in headless mode
+driver = webdriver.Chrome(options=chrome_options)
 
-for option in options:
-    chrome_options.add_argument(option)
+matchs_by_date = {"listMatchs": {}}
 
-    
-driver = webdriver.Chrome(options = chrome_options)
+for page_num in range(1, 5):  # Số trang cần lấy (tùy chọn)
+    # Tạo URL cho mỗi trang
+    url = (
+        f"https://www.hkfa.com/en/competitions/fixtures?year=2023-2024&page={page_num}"
+    )
+    driver.get(url)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".container"))
+    )
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    # Lấy thông tin về các trận đấu
+    match_items = soup.find_all(class_="mb-5")
+    # Trích xuất thông tin từ từng trận đấu
+    for match_item in match_items:
+        match_info_parts = match_item.text.strip().split("\n")
+        if len(match_info_parts) >= 9:
+            date = match_info_parts[0].strip()
 
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+            match_obj = {
+                "match_time": match_info_parts[4].strip(),
+                "home_score": match_info_parts[2].strip(),
+                "guest_score": match_info_parts[3].strip(),
+                "match_id": 41185,
+                "match_date": match_info_parts[0].strip(),
+                "match_round": "d",
+                "match_ticket": match_info_parts[-1].strip(),
+                "matchStatus": "g",
+                "match_status": "e",
+                "venue_id": "W",
+                "live_icon": [],
+                "Venue": {"venue_id": "W", "VenueName": "aF"},
+            }
 
+            if date not in matchs_by_date["listMatchs"]:
+                matchs_by_date["listMatchs"][date] = []
+
+            matchs_by_date["listMatchs"][date].append(match_obj)
+
+    print(f"Lấy dữ liệu từ trang {page_num}")
+
+# Đóng trình duyệt
+driver.quit()
+
+# Lưu dữ liệu vào file JSON
+output_file = "match_data.json"
+with open(output_file, "w") as json_file:
+    json.dump(matchs_by_date, json_file, indent=4)
+
+print(f"Dữ liệu đã được lưu vào file: {output_file}")
