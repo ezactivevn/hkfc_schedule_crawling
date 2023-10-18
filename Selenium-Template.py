c13536c25e21d6e46c580b424ee0075ef3b2b944
Hoang Phu <hoangphu@MacBook-Pro-cua-Hoang.local>
2023-10-18 17:29:00 +0700

    update crawl data

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options  # Import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
 from bs4 import BeautifulSoup
 
 # Create ChromeOptions and set it to run in headless mode
@@ -46,70 +48,84 @@ for page_num in range(array_pages[0], array_pages[-1] + 1):
     match_items = driver.find_elements(By.CSS_SELECTOR, ".container div.mb-5")
 
     for match_item in match_items:
-        match_info_parts = match_item.text.strip().split("\n")
-        date_name= driver.find_element(By.CSS_SELECTOR, ".container div.mb-5 div.strong-title")
-        date_name = date_name.text
-
-        match_details = driver.find_elements(
-            By.XPATH, f'//div[@class="mb-5"]/div[text()="{date_name}"]/parent::div/div[@class="items"]'
+        date_of_match = match_item.find_element(
+            By.CSS_SELECTOR, "div.strong-title"
+        ).text
+        
+        matchs_by_date = match_item.find_element(
+            By.CSS_SELECTOR, "div.items"
         )
 
-        for match_detail in match_details:
-            match = match_detail.text.strip().split("\n")
+        for match in matchs_by_date.find_elements(By.CSS_SELECTOR, "div.single-fixture"):
+            data = {
+                "date": date_of_match,
+                "home_team": "",
+                "away_team": "",
+                "match_time": "",
+                "venue": "",
+                "home_score": "",
+                "away_score": ""
+            }
+
+            try:
+                data["home_team"] = match.find_element(
+                    By.CSS_SELECTOR, "div.home-team"
+                ).text
+            except NoSuchElementException:
+                pass
+
+            try:
+                data["away_team"] = match.find_element(
+                    By.CSS_SELECTOR, "div.guest-team"
+                ).text
+            except NoSuchElementException:
+                pass
+
+            try:
+                data["match_time"] = match.find_element(
+                    By.CSS_SELECTOR, "div.clock"
+                ).text
+            except NoSuchElementException:
+                pass
+
+            try:
+                data["venue"] = match.find_element(
+                    By.CSS_SELECTOR, "div.venue-wrapper"
+                ).text
+            except NoSuchElementException:
+                pass
+
+            try:
+                data["tournament"] = match.find_element(
+                    By.CSS_SELECTOR, "div.league-wrapper"
+                ).text
+            except NoSuchElementException:
+                pass
+
+            try:
+                data["home_score"] = match.find_element(
+                    By.CSS_SELECTOR, "div.score:nth-child(1)"
+                ).text
+            except NoSuchElementException:
+                pass
+
+            try:
+                data["away_score"] = match.find_element(
+                    By.CSS_SELECTOR, "div.score:nth-child(2)"
+                ).text
+            except NoSuchElementException:
+                pass
+
+            results.append(data)
+                
+
+            
 
 
-            if match[0].strip() == "DELAY":
-                match_obj = {
-                    "date": date_name,
-                    "home_score": match[0].strip(),
-                    "away_score": match[0].strip(),
-                    "match_time": match[1].strip(),
-                    "home_team": match[2].strip(),
-                    "away_team": match[3].strip(),
-                }
-            elif not match[0].isnumeric():
-                match_obj = {
-                    "date": date_name,
-                    "home_score": "",
-                    "away_score": "",
-                    "match_time": match[0].strip(),
-                    "home_team": match[1].strip(),
-                    "away_team": match[2].strip(),
-                }
-            else:
-                match_obj = {
-                    "date": date_name,
-                    "home_score": match[0].strip(),
-                    "away_score": match[1].strip(),
-                    "match_time": match[2].strip(),
-                    "home_team": match[3].strip(),
-                    "away_team": match[4].strip(),
-                }  
 
-            keywords = ["Park", "Training Center", "Playground", "Stadium", "Ground"]
-            for i in range(4, len(match)):
-                contains_keyword = any(
-                    keyword in match[i] for keyword in keywords
-                )
-                if contains_keyword:
-                    match_obj["venue"] = match[i].strip()
-                    break
 
-            keywords = ["Cup", "League", "Division"]
-            for i in range(4, len(match)):
-                contains_keyword = any(
-                    keyword in match[i] for keyword in keywords
-                )
-                if contains_keyword:
-                    match_obj["tournament"] = match[i].strip()
-                    break
-
-            keywords = ["$", "T.B.C"]
-            if any(keyword in match[-1].strip() for keyword in keywords):
-                match_obj["ticket"] = match[-1].strip()
-
-            results.append(match_obj)
-
+        
+       
     print(f"Lấy dữ liệu từ trang {page_num}")
 
 # Đóng trình duyệt
